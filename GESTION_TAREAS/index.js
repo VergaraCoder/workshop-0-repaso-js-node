ca4d654d93201d0a32620d108661517189a113f4
jhonatan <jhonatanvergara072@gmail.com>
2024-07-05 09:04:21 -0500

    melo

--- GESTION_TAREAS/index.js
+++ GESTION_TAREAS/index.js
@@ -1,92 +1,92 @@
+/*Lo unico que copie de nicolas fue el html inicial de resto nada mas*/
 
-class newTask{
-    constructor(id,description){
-        this.id=id;
-        this.description=description;
+
+
+class newTask{   //Creamos una clase encargada de darle un estado inicial de sus instancias(una copia de la clase o ejemplar );
+    constructor(id,description){   //Declaramos el metodo constructor que se encarga de darle ciertas propiedades al objeto instanciado
+        this.id=id;  //Estas caracteristicas son un id
+        this.description=description;  // y una descripcion de la nueva tarea
     }
-    addTask(){
-        localStorage.setItem(this.id,this.description);
-        location.reload();
+    addTask(){  //creamos un metodo(funcion dentro de clase) para realizar un comportamiento y este es agregar la tarea el local storage
+        localStorage.setItem(this.id,this.description);  // le pasamos como parametro el id y la descripcion dados en el objeto que fue instanciado en ese momento
+        location.reload();  // recargamos la pagina 
     }
 }
 
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded',()=>{  // Damos un evento al documento para que despues de cargar todos los elementos del mismo hagamos todo lo siguiente
 
-    const $button=document.getElementById('add-task');
+    const $button=document.getElementById('add-task');  // Seleccionamos un boton que se encargara de añadir una nueva tarea
 
-    $button.addEventListener("click",(e)=>{
+    $button.addEventListener("click",(e)=>{  //damos un evento de click al boton anterior 
 
-        const valueTask=document.getElementById('new-task').value;
-        let count=localStorage.length;
+        const valueTask=document.getElementById('new-task').value;  // Seleccionamos el input que contiene la descripcion de la tarea y extraemos su valor
+        let count=localStorage.length;  // Contamos cuantos elementos tiene actualmente el local Storage
 
-        console.log(count);
+        console.log(count);  // pequeño console que use para verificar 
         
-        const object=new newTask(`task${count+=1}`,valueTask);
-        object.addTask();
+        const object=new newTask(`task${count+=1}`,valueTask);  // instanciamos un nuevo objeto de la clase creada al incio del fichero y pasamos como parametro la palabra task junto con el numero total de elementos que hay en el localStorage sumandole 1 , y como valor de esa clave el valor del input que contenia la descripcion de la tarea
+        object.addTask(); // una vez instanciado un objeto de la clase activamos el metodo para añadir esa tarea al localStorage
 
     });
 
 
-    const $elementHTML=document.getElementById('task-list');
-    showData($elementHTML);
+    const $elementHTML=document.getElementById('task-list');  // seleccionamos un elemento UL que es quien contendra todas las tareas que hay en el local storage
+    showData($elementHTML);  // llamamos a una funcion encargada de dibujar dichas tareas subidas en el localStorage y pasamos como parametro el UL($elementHTML)
    
 });
 
 
 
-function showData($HTML){
-    for(let i=0; i<localStorage.length;i++){
+function showData($HTML){  // aca Creamos la funcion para dibujar los elementos en el html y le pasamos como parametro un elmento html que en este caso es un UL
+    for(let i=0; i<localStorage.length;i++){   //inicamos una iteracion de elementos tomando para el nuemro de iteraciones el total de las tareas que existen en el localStorage
 
-        const data2=localStorage.key(i)
-        const data=localStorage.getItem(data2);
+        const data2=localStorage.key(i) // visualizamos que clave hay por ejemplo en la posicion 0 en el local storage 
+        const data=localStorage.getItem(data2); // Esa clave data la usamos para extraer el valor de ese elemento 
 
-        if(data2){
-            const $button=document.createElement('BUTTON');
-            const $button2=document.createElement('BUTTON');
-            const $li=document.createElement('LI');
-            const $input=document.createElement('INPUT');
-            $input.type="checkbox";
+        if(data2){  // En el caso de que en cierta posicion si exista una clave haremos lo siguiente
+            const $button=document.createElement('BUTTON'); // creamos un elemento html en este caso un boton para eliminar
+            const $button2=document.createElement('BUTTON'); // creamos otro boton para actualizar 
+            const $li=document.createElement('LI');   //creamos un elemento de lista 
 
-            $li.dataset.id=data2;
-            $li.dataset.task=data;
-            $li.textContent=data;
-            $button.textContent="eliminar";
-            $button2.textContent="Actualizar";
+            $li.dataset.id=data2;  // Aca establecer un atributo data- para que el elemento LI contenga cierta infromacion de manera interna para posterior uso en este caso el metado dato se llama id (data-id)
+            $li.dataset.task=data; // Aca establecemos otro metadato para en este caso almacenar la descripcion de la tarea (data-task)
+            $li.textContent=data; //Establecemos como contenido visual el valor de data en el momento
+            $button.textContent="eliminar"; // Al primer boton le damos como texto eliminar
+            $button2.textContent="Actualizar";// Al segundo boton le damos como valor actualizar
         
-            $HTML.appendChild($li);
-            $li.appendChild($button);
-            $li.appendChild($button2);
-            $li.appendChild($input);
+            $HTML.appendChild($li);  //El elemento HTML de parametro que sera un UL sera el padre del elemento LI
+            $li.appendChild($button); //El elemento LI sera el padre del primer boton creado(boton de eliminar)
+            $li.appendChild($button2); //El elemento LI sera el padre del segundo boton creado(boton de actualizar)
 
-            $button.addEventListener("click",()=>{
-                deletetaks($li);
+            $button.addEventListener("click",()=>{  // para el primer boton(Boton de eliminar) le damos un evento que cuando se activa
+                deletetaks($li);//llamamos a una funcion encargada eliminar la tarea y pasamos como parametro el elemento li que recordemos que que tiene los metadatos internos (el id, y la descripcion)
             })
 
-            $button2.addEventListener("click",()=>{
-                updateData($li);
+            $button2.addEventListener("click",()=>{  // Al segundo boton(Boton de actualizar) le damos como un evento tambien
+                updateData($li); // llamamos una funcion encargada de realizar la actualizacion de una tarea en especifico
             });
         }
     }
 }
 
 
-function deletetaks($elementList){
-    localStorage.removeItem($elementList.dataset.id);
-    console.log($elementList.dataset.id);
-    location.reload();
+function deletetaks($elementList){  //creamos la funcion encargada de eliminar una tarea y como parametro el elemento LI(Que es respresentado en la funcion como $elementList)
+    localStorage.removeItem($elementList.dataset.id);  //En el local Storage hacemos activamos en metodo removeItem y como parametro le pasamos el id(o la clave) de un cierto valor , y recordemos que en $elementList tiene 2 metadatos y uno de esos es el id (o la clave) del mismo
+    location.reload(); // recargamos la pagina para que la funcion que dibuja tanbien se actualice y quite de la pagina la tarea eliminada
 }
 
-function updateData($elementList){
-    const $buttonUpdate=document.getElementById('update-task');
-    $buttonUpdate.style.display="block";
 
-    const $inputWithData=document.getElementById('new-task');
+function updateData($elementList){  // funcion encargada de actualizar una tarea en especifico
+    const $buttonUpdate=document.getElementById('update-task'); // Seleccionamos otro boton 
+    $buttonUpdate.style.display="block"; // Este boton incialmente estaba oculto por cual al momento de llmar esta funcion estara a la vista
 
-    $inputWithData.value=$elementList.dataset.task;
-
-    $buttonUpdate.addEventListener("click",()=>{
-        localStorage.setItem($elementList.dataset.id,$inputWithData.value);
-        location.reload();
+    const $inputWithData=document.getElementById('new-task');  // seleccionamos el input que contendra la descripcion actualizada de la tarea 
+ 
+    $inputWithData.value=$elementList.dataset.task;  //Al input le damos un valor que sea la descripcion y que esta descripcion esta en el metadato interno de($elementList) para que el usuario vea claramente que parte de la descripcion desea actualizar.
+ 
+    $buttonUpdate.addEventListener("click",()=>{ // creamos un evento de click para el boton que antes estaba oculto pero que ahora es visible
+        localStorage.setItem($elementList.dataset.id,$inputWithData.value); // y cuando se le click cogemos el local Storage y reescribimos el dato que tiene la misma clave ($elementList.dataset.id) pero con el nuevo valor ($inputWithData.value)
+        location.reload(); // recargamos la pagina para que visualmente tambien se actualice
     });
 }
--- METODOS_ARRAYS/index.js
+++ METODOS_ARRAYS/index.js
@@ -11,106 +11,109 @@ const products = [
     { id: 9, name: 'Sunglasses', category: 'Accessories', price: 30, stock: 35 }
 ];
 
-const $container=document.querySelector(".containerProducts");
+const $container=document.querySelector(".containerProducts"); //container que contendra y dibujara los productos
 
 const sumTotal=(products)=>{
-    let sum=0;
-    const resultado=products.reduce((acc,item)=>{
-        sum=acc+item.price;
-        return sum;
-    },0)
-    return resultado;
+    let sum=0;                                          //Iniamos una varibale en cero 
+    const resultado=products.reduce((acc,item)=>{ //Activamos reduce pasandole 2 parametros que son el acomulador y el objeto actual 
+        sum=acc+item.price;         //La varibale suma empieza a tomar valor y este es la suma de la propiedad price del elemento actual , mas el acomulador que en un principio en cero
+        return sum;   // retornamos el valor total de la suma antes hecha
+    },0) // este parametro es opcional e indica el valor por el cual se empezara la operacion , solo lo puse para estar conciente que podia incluir otro valor opcional ahi
+    return resultado;   //Retornamos el valor de la suma que es todo lo que hicimos dentro de reduce
 }
 
 
-const filterCategories=(products,option)=>{
-    const filters=products.filter((item)=>{
-        return item.category==option;
-    });
-    return filters;
+const filterCategories=(products,option)=>{ // Pasamos 2 parametros a la funcion que son la lista de productos, y la opcion de categoria
+    const filters=products.filter((item)=>{ //Activamos el metodo filter y como parametro el elemento que se recorre actualmente
+        return item.category==option;   //Ponemos una condicion para que se vayan filtrando los objetos y debe cumplir con que la categoria del objeto actual debe ser igual a la opcion del parametro
+    }); 
+    return filters;   // Finalmente retornamos el valor de (filters)-> Que son todos los objeto que cumplieron la condicion interna de filter
 }
 
 
-const showFilter=(data)=>{
-    $container.innerHTML="";
-    for(x of data){
-        $container.innerHTML+=`
+const showFilter=(data)=>{  //Funcion encargada de mostrar o dibujar los productos filtrados , pasamos como parametro dichos objetos
+    $container.innerHTML="";        //Todo lo que tenga container lo borramos 
+    for(x of data){     //Iniciamos un for que nos recorreo los objetos, como es un objeto lo mas apropiado seria usar in pero lo voy a dejar asi
+        $container.innerHTML+=` 
         <div class="product">
             <h1>Nombre Producto: ${x.name}</h1>
             <h2>Categoria: ${x.category}</h2>
-            <h2>Precio: ${x.price}</h2>
+            <h2>Precio: ${x.price}</h2>                             
             <h2>Displonibles: ${x.stock}</h2>
         </div>
         `;
+
+        //En lo anteiror solo dibujamos las propiedades dentro de etiquetas html para mejorar visualmente en contenido y mas facil de maniupular ala hora de dar estilos
+
     }
 }
 
-const sumCategory=(data,option)=>{
-    let suma=0;
-    const resultado= data.reduce((acc,item)=>{
-        if(item.category==option){
-            suma=acc+item.price;
-        }
-        return suma;
-    },0);
+const sumCategory=(data)=>{  // Funcion encargada de sumar todos los productos de una categoria, le pasamos la data ya filtrada comon parametro
+    let suma=0;  // iniciamos la varibale en 0 para irla sumando
+    const resultado= data.reduce((acc,item)=>{//El acoumulador del resultado con el elemento acutal como parametros
+        return suma=acc+item.price;  //Retornamos dicha suma
+    },0);  // nuevamente el parametro opcional pero por gusto lo dejo
 
-    return resultado;
+    return resultado;  //retornamos el valor de la suma dado del metodo reduce
 }
 
-const searchProductByName=(data,productName)=>{
-    const result= data.find((item)=>{
-        return item.name.toLowerCase()==productName.toLowerCase();;
+const searchProductByName=(data,productName)=>{  // funcion encargada de buscar los productos por nombre le paso como parametro , los productos , y el nombre del producto
+    const result= data.find((item)=>{ // Inicamos el metodo find pasando como parametro el elemento que esta siendo recorrido actualmente
+        return item.name.toLowerCase()==productName.toLowerCase();  // convertimos tanto el nombre del producto , como el nombre del producto dado por el usuario si coinciden y hacer una busqueda mas amigable y retornamos dicho objeto
     });
-    return result;
+    return result;  // retornamos el objeto que concide con la condicion dicha dentro de result
 }
 
 
-const verifyStockProducts=(data)=>{
-    const resultado=data.every((item)=>{
-        return item.stock !==0;
+const verifyStockProducts=(data)=>{  // funcion encargad de verificar si todos los productos estan disponibles actualmente
+    const resultado=data.every((item)=>{ // llamamos el metodo every para retornar un valor booleano si todos los elementos del array cumplen con una condicion
+        return item.stock > 0; //Esta en la condicion y para verificar si todos los productos todavia estan disponibles verificando que su stock sea mayor que cero
     });
-    return resultado;
+    return resultado;  // retornamos el boleano
 }
 
-const $select=document.getElementById("categories");
+const $select=document.getElementById("categories");   //elemento html que contiene el valor seleccionado en las categorias
 
-const $buttonSumCategory=document.querySelector(".buttonSum");
+const $buttonSumCategory=document.querySelector(".buttonSum");  //boton encargado de hacer cumplir la suma de todos los precios de una categoria
 
-const $buttonSumTotal=document.querySelector(".buttonSum2");
+const $buttonSumTotal=document.querySelector(".buttonSum2"); // boton encargado de hacer la suma del precio de todos los productos
 
-const $inputSearch=document.getElementById("searchName");
+const $inputSearch=document.getElementById("searchName"); // input donde se escribira el nombre del producto que se desea encontrar
 
-const $buttonSearch=document.querySelector(".search");
+const $buttonSearch=document.querySelector(".search"); // boton que activa la busqueda dependiendo del valor del inpur anterior
 
-const $buttonVerify=document.querySelector(".verify");
+const $buttonVerify=document.querySelector(".verify"); // boton que verifica si todos los productos estan disponibles
 
-$select.addEventListener("change",()=>{
-    const result=filterCategories(products,$select.value);
+$select.addEventListener("change",()=>{  //Hacemos un evento para $select de tipo change que se activa cada vez que este cambie su valor
+    const result=filterCategories(products,$select.value); //llamamos a la funcion para filtrar por categorias y pasamos como parametro el objeto de productos y el valor de $select (ESTE VALOR ES LA CATEGORIA SELECCIONADA)
 
-    showFilter(result);
+    showFilter(result);  // Llamamos a la funcion encargada de pintar todos los productos
 });
 
 
-$buttonSumCategory.addEventListener("click",()=>{
-    const result=sumCategory(products,$select.value);
+$buttonSumCategory.addEventListener("click",()=>{  // activamos un evento de click para el boton encargado de activar la suma de todos los precios de una categoria
+    const filter=filterCategories(products,$select.value); //llamamos a la funcion que filtra dichos productos pasando como parametro del objeto de productos y el valor actual de la categoria($select.value)
+    const result=sumCategory(filter);  //llamamos a la funcion encargada de hacer la suma de los objetos ya filtrados en la funcion anterior
     $container.innerHTML=`
         <h1>El precio total de esta categoria es : ${result}</h1>
     `;
+    //Dibujamos el precio 
 });
 
 
-$buttonSumTotal.addEventListener("click",()=>{
-    const result=sumTotal(products);
+$buttonSumTotal.addEventListener("click",()=>{    // evento de click para boton que activa la suma de los precios de todas las categorias
+    const result=sumTotal(products);   //llamamos a la funcion encargada de hacer la suma total de todo el objeto products decalarado al inicio del fichero
     $container.innerHTML=`
         <h1>El precio de todas las categorias es : ${result}</h1>
     `;
+    //Dibujamos la suma total de todos los productos
 });
 
-$buttonSearch.addEventListener("click",()=>{
-    try{
-        const result=searchProductByName(products,$inputSearch.value);
+$buttonSearch.addEventListener("click",()=>{   //hacemos evento de click para el boton encargado de activar la busqueda de producto or nombre
+    try{  //en este caso pude haberlo hecho con condicionales pero deicido hacer con manejo de excepciones
+        const result=searchProductByName(products,$inputSearch.value);  // llamamos a la funcion encargada de filtrarme el producto que cumpla con el nombre dado por el usuario
 
-        $container.innerHTML=`
+        $container.innerHTML=` 
         <div class="product">
             <h1>Nombre Producto: ${result.name}</h1>
             <h2>Categoria: ${result.category}</h2>
@@ -118,19 +121,20 @@ $buttonSearch.addEventListener("click",()=>{
             <h2>Displonibles: ${result.stock}</h2>
         </div>
         `;
+        //Dibujamos dicho producto
     }
-    catch(err){
-        $container.innerHTML=`<h1>No se a encontrado el producto</h1>`;
-    }
+    catch(err){  // en el caso de que pase algo , ya sea que el usuario de un nombre que no es ingrese algo raro , activamos el catch 
+        $container.innerHTML=`<h1>No se a encontrado el producto</h1>`;  
+    }    // y dibujamos que no se encontro el productos  
 });
 
-$buttonVerify.addEventListener("click",()=>{
-    const result= verifyStockProducts(products);
-    if(result){
+$buttonVerify.addEventListener("click",()=>{    //Boton que verifica si todos los productos estan disponibles
+    const result= verifyStockProducts(products);   // llamamos a la funcion que se encarga de realizar dicha verificacion
+    if(result){ //En el caso de que sea true dibujamos el siguiente
         $container.innerHTML=`
             <h1>Actualmente SI estan todos disponibles</h1>
       `;
-    }else{
+    }else{ //En el caso de que sea false dibujamos lo siguiente
         $container.innerHTML=`
         <h1>Actualmente NO estan todos disponibles</h1>
         `;
